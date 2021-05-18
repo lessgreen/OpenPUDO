c82ef505eed627423855fa2310c35cd1e3bb7f1d
Francesco Magno <francesco@magno.cc>
2021-05-18 23:01:55 +0200

    moved address to separate api

--- src/main/java/less/green/openpudo/persistence/service/UserService.java
+++ src/main/java/less/green/openpudo/persistence/service/UserService.java
@@ -7,13 +7,9 @@ import javax.transaction.Transactional;
 import less.green.openpudo.cdi.service.CryptoService;
 import static less.green.openpudo.common.StringUtils.sanitizeString;
 import less.green.openpudo.common.dto.UserSecret;
-import less.green.openpudo.persistence.dao.AddressDao;
-import less.green.openpudo.persistence.dao.UserAddressDao;
 import less.green.openpudo.persistence.dao.UserDao;
 import less.green.openpudo.persistence.dao.UserProfileDao;
-import less.green.openpudo.persistence.model.TbAddress;
 import less.green.openpudo.persistence.model.TbUser;
-import less.green.openpudo.persistence.model.TbUserAddress;
 import less.green.openpudo.persistence.model.TbUserProfile;
 import less.green.openpudo.rest.dto.DtoMapper;
 import less.green.openpudo.rest.dto.auth.RegisterRequest;
@@ -32,10 +28,6 @@ public class UserService {
     UserDao userDao;
     @Inject
     UserProfileDao userProfileDao;
-    @Inject
-    AddressDao addressDao;
-    @Inject
-    UserAddressDao userAddressDao;
 
     public TbUser createUser(RegisterRequest req) {
         Date now = new Date();
@@ -60,25 +52,6 @@ public class UserService {
         userProfile.setSsn(sanitizeString(req.getUserProfile().getSsn()));
         userProfileDao.persist(userProfile);
         userProfileDao.flush();
-        TbAddress address = new TbAddress();
-        address.setCreateTms(now);
-        address.setUpdateTms(now);
-        address.setStreet(sanitizeString(req.getAddress().getStreet()));
-        address.setStreetNum(sanitizeString(req.getAddress().getStreetNum()));
-        address.setZipCode(sanitizeString(req.getAddress().getZipCode()));
-        address.setCity(sanitizeString(req.getAddress().getCity()));
-        address.setProvince(sanitizeString(req.getAddress().getProvince()));
-        address.setCountry(sanitizeString(req.getAddress().getCountry()));
-        address.setNotes(sanitizeString(req.getAddress().getNotes()));
-        address.setLat(req.getAddress().getLat());
-        address.setLon(req.getAddress().getLon());
-        addressDao.persist(address);
-        addressDao.flush();
-        TbUserAddress userAddress = new TbUserAddress();
-        userAddress.setUserId(user.getUserId());
-        userAddress.setAddressId(address.getAddressId());
-        userAddressDao.persist(userAddress);
-        userAddressDao.flush();
         return user;
     }
 
