3e2788fd9fec72f0acf3e9aea5cfad1061d16b5f
Francesco Magno <francesco@magno.cc>
2021-07-17 16:13:27 +0200

    refactoring package API

--- src/main/java/less/green/openpudo/rest/dto/pack/Package.java
+++ src/main/java/less/green/openpudo/rest/dto/pack/Package.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import java.util.Date;
 import java.util.List;
 import java.util.UUID;
-import less.green.openpudo.persistence.dao.usertype.PackageStatus;
 import lombok.Data;
 import org.eclipse.microprofile.openapi.annotations.media.Schema;
 
@@ -27,9 +26,6 @@ public class Package {
 
     @Schema(readOnly = true)
     private Long userId;
-
-    @Schema(readOnly = true)
-    private PackageStatus packageStatus;
 
     @Schema(readOnly = true)
     private UUID packagePicId;
