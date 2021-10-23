607b39d0428118c10c7270eecc1091e5138a9a3f
Francesco Magno <francesco@magno.cc>
2021-10-23 12:17:56 +0200

    password reset page with actual calls

--- src/main/java/less/green/openpudo/rest/resource/AuthResource.java
+++ src/main/java/less/green/openpudo/rest/resource/AuthResource.java
@@ -50,7 +50,7 @@ public class AuthResource {
     // (?=.{8,}) -> at least 8 char
     // (?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]) -> at least one lowercase latter, one uppercase, one number
     private static final String PASSWORD_REGEX = "^(?=.{8,}$)(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).+$";
-    private static final Pattern PASSWOD_PATTERN = Pattern.compile(PASSWORD_REGEX);
+    private static final Pattern PASSWORD_PATTERN = Pattern.compile(PASSWORD_REGEX);
     private static final String EMAIL_REGEX = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)])";
     private static final Pattern EMAIL_PATTERN = Pattern.compile(EMAIL_REGEX);
 
@@ -122,7 +122,7 @@ public class AuthResource {
         req.setPhoneNumber(phoneNumber);
 
         String password = req.getPassword();
-        if (!PASSWOD_PATTERN.matcher(password).matches()) {
+        if (!PASSWORD_PATTERN.matcher(password).matches()) {
             throw new ApiException(ApiReturnCodes.INVALID_REQUEST, localizationService.getMessage(language, "error.auth.password_too_easy"));
         }
 
@@ -294,7 +294,7 @@ public class AuthResource {
             throw new ApiException(ApiReturnCodes.INVALID_REQUEST, localizationService.getMessage(language, "error.empty_mandatory_field", "login"));
         } else if (isEmpty(req.getOtp())) {
             throw new ApiException(ApiReturnCodes.INVALID_REQUEST, localizationService.getMessage(language, "error.empty_mandatory_field", "otp"));
-        } else if (isEmpty(req.getLogin())) {
+        } else if (isEmpty(req.getNewPassword())) {
             throw new ApiException(ApiReturnCodes.INVALID_REQUEST, localizationService.getMessage(language, "error.empty_mandatory_field", "newPassword"));
         }
 
@@ -323,7 +323,7 @@ public class AuthResource {
             delayFailureResponse();
             throw new ApiException(ApiReturnCodes.INVALID_CREDENTIALS, localizationService.getMessage(language, "error.auth.invalid_credentials"));
         }
-        if (!PASSWOD_PATTERN.matcher(req.getNewPassword()).matches()) {
+        if (!PASSWORD_PATTERN.matcher(req.getNewPassword()).matches()) {
             throw new ApiException(ApiReturnCodes.INVALID_REQUEST, localizationService.getMessage(language, "error.auth.password_too_easy"));
         }
 
