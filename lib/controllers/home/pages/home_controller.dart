92331ed131979d83deadb88c7a19e52ec3672eb4
Francesco La Forgia <tecfra98@gmail.com>
2022-01-24 14:36:41 +0100

    Update homepage
    - Correct Ui implementation of homepage with cupertino library

--- lib/controllers/home/pages/home_controller.dart
+++ lib/controllers/home/pages/home_controller.dart
@@ -7,11 +7,14 @@
 
 // ignore_for_file: unused_import
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:qui_green/commons/widgets/base_page.dart';
 import 'package:qui_green/controllers/home/viewmodel/home_controller_viewmodel.dart';
+import 'package:qui_green/controllers/profile/pages/profile_controller.dart';
 import 'package:qui_green/models/page_type.dart';
+import 'package:qui_green/resources/res.dart';
 import 'package:qui_green/resources/routes_enum.dart';
 
 class HomeController extends StatefulWidget {
@@ -25,25 +28,75 @@ class _HomeControllerState extends State<HomeController> {
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
-      providers: [ChangeNotifierProxyProvider0<HomeControllerViewModel?>(
-          create: (context) => HomeControllerViewModel(),
-          update: (context, viewModel) => viewModel),],
+      providers: [
+        ChangeNotifierProxyProvider0<HomeControllerViewModel?>(
+            create: (context) => HomeControllerViewModel(),
+            update: (context, viewModel) => viewModel),
+      ],
       child: Consumer<HomeControllerViewModel>(builder: (_, viewModel, __) {
-        return BasePage(
-          title: 'QuiGreen',
-          showBackIcon: false,
-          onPressedBack: () => Navigator.of(context).pop(),
-          index: 0,
-          headerVisible: true,
-          icon: const Icon(Icons.account_circle),
-          onPressedIcon: () =>
-              Navigator.of(context).pushReplacementNamed(Routes.profile),
-          body: Center(
-              child: Text(
-            'Non ci sono ancora consegne in attesa per te!',
-            textAlign: TextAlign.center,
-            style: TextStyle(color: Colors.grey.shade600),
-          )),
+        return Material(
+          child: CupertinoTabScaffold(
+            tabBar: CupertinoTabBar(
+              items: const [
+                BottomNavigationBarItem(icon: Icon(Icons.home)),
+                BottomNavigationBarItem(icon: Icon(Icons.map)),
+              ],
+            ),
+            tabBuilder: (innerContext, index) {
+              switch (index) {
+                case 1:
+                  return CupertinoTabView(
+                    routes: {
+                      '/': (context) {
+                        return CupertinoPageScaffold(
+                          navigationBar: CupertinoNavigationBar(
+                            backgroundColor: AppColors.primaryColorDark,
+                            middle: Text(
+                              'Pudo',
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .headline6
+                                  ?.copyWith(color: Colors.white),
+                            ),
+                          ),
+                          child: const Center(
+                            child: Text("Pudo"),
+                          ),
+                        );
+                      },
+                    },
+                  );
+                default:
+                  return CupertinoTabView(
+                    routes: {
+                      '/': (context) {
+                        return CupertinoPageScaffold(
+                            navigationBar: CupertinoNavigationBar(
+                              backgroundColor: AppColors.primaryColorDark,
+                              middle: Text(
+                                'Home',
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .headline6
+                                    ?.copyWith(color: Colors.white),
+                              ),
+                              trailing: IconButton(
+                                icon: const Icon(Icons.account_circle_rounded,color: Colors.white,),
+                                onPressed: ()=>Navigator.of(context).pushNamed(Routes.profile),
+                              ),
+                            ),
+                            child: const Center(
+                              child: Text("Home"),
+                            ));
+                      },
+                      '/profile': (context) {
+                        return const ProfileController();
+                      },
+                    },
+                  );
+              }
+            },
+          ),
         );
       }),
     );
--- lib/controllers/profile/pages/profile_controller.dart
+++ lib/controllers/profile/pages/profile_controller.dart
@@ -5,13 +5,11 @@
 //  Created by Costantino Pistagna on 04/01/2022.
 //  Copyright © 2022 Sofapps. All rights reserved.
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:qui_green/commons/widgets/base_page.dart';
 import 'package:qui_green/controllers/profile/viewmodel/profile_controller_viewmodel.dart';
 import 'package:qui_green/resources/res.dart';
-import 'package:qui_green/resources/routes_enum.dart';
-
 class ProfileController extends StatefulWidget {
   const ProfileController({Key? key}) : super(key: key);
 
@@ -23,108 +21,118 @@ class _ProfileControllerState extends State<ProfileController> {
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
-      providers: [ChangeNotifierProxyProvider0<ProfileControllerViewModel?>(
-          create: (context) => ProfileControllerViewModel(),
-          update: (context, viewModel) => viewModel),],
+      providers: [
+        ChangeNotifierProxyProvider0<ProfileControllerViewModel?>(
+            create: (context) => ProfileControllerViewModel(),
+            update: (context, viewModel) => viewModel),
+      ],
       child: Consumer<ProfileControllerViewModel>(builder: (_, viewModel, __) {
-        return BasePage(
-          title: 'Il tuo profilo',
-          showBackIcon: true,
-          onPressedBack: () => Navigator.of(context).pushReplacementNamed(Routes.home),
-          headerVisible: true,
-          icon: const Icon(Icons.edit),
-          onPressedIcon: () => null,
-          index: 0,
-          body: Column(
-            children: [
-              const SizedBox(height: 20),
-              const Center(
-                child: CircleAvatar(
-                  radius: 80,
-                  backgroundImage: NetworkImage(
-                      'https://i1.sndcdn.com/avatars-TlbXx1BArSO2iBM1-r5ax8A-t500x500.jpg'),
+        return Material(
+          child: CupertinoPageScaffold(
+              navigationBar: CupertinoNavigationBar(
+                backgroundColor: AppColors.primaryColorDark,
+                middle: Text(
+                  'Il tuo profilo',
+                  style: Theme.of(context)
+                      .textTheme
+                      .headline6
+                      ?.copyWith(color: Colors.white),
+                ),
+                leading: CupertinoNavigationBarBackButton(
+                  color: Colors.white,
+                  onPressed: () => Navigator.of(context).pop(),
                 ),
               ),
-              const SizedBox(
-                height: 10,
-              ),
-              const Text(
-                'Jennifer Seed',
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 17),
-              ),
-              Text(
-                'Utente dal 21/11/2021',
-                style: TextStyle(fontSize: 12, color: Colors.grey.shade500),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              RichText(
-                textAlign: TextAlign.center,
-                text: TextSpan(
-                  text: '',
-                  style: Theme.of(context).textTheme.bodyText2,
-                  children: const [
-                    TextSpan(text: "Hai usato il servizio di QuiGreen "),
-                    TextSpan(
-                        text: "123",
-                        style: TextStyle(
-                            color: AppColors.accentColor,
-                            fontWeight: FontWeight.bold)),
-                    TextSpan(text: " volte,"),
-                    TextSpan(text: " contribuendo a ridurre di "),
-                    TextSpan(
-                        text: "456kg",
-                        style: TextStyle(
-                            color: AppColors.accentColor,
-                            fontWeight: FontWeight.bold)),
-                    TextSpan(text: " le emissioni di CO2")
-                  ],
-                ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              const Divider(color: Colors.grey),
-              GestureDetector(
-                onTap: () => viewModel.goToPudos(context),
-                child: Row(
-                  children: const [
-                    SizedBox(width: 10),
-                    Icon(
-                      Icons.person_pin_circle,
-                      color: AppColors.cardColor,
+              child: Column(
+                children: [
+                  const SizedBox(height: 20),
+                  const Center(
+                    child: CircleAvatar(
+                      radius: 80,
+                      backgroundImage: NetworkImage(
+                          'https://i1.sndcdn.com/avatars-TlbXx1BArSO2iBM1-r5ax8A-t500x500.jpg'),
                     ),
-                    SizedBox(width: 10),
-                    Text("I tuoi pudo"),
-                    Spacer(),
-                    Icon(
-                      Icons.keyboard_arrow_right,
-                      color: AppColors.cardColor,
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  const Text(
+                    'Jennifer Seed',
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 17),
+                  ),
+                  Text(
+                    'Utente dal 21/11/2021',
+                    style: TextStyle(fontSize: 12, color: Colors.grey.shade500),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  RichText(
+                    textAlign: TextAlign.center,
+                    text: TextSpan(
+                      text: '',
+                      style: Theme.of(context).textTheme.bodyText2,
+                      children: const [
+                        TextSpan(text: "Hai usato il servizio di QuiGreen "),
+                        TextSpan(
+                            text: "123",
+                            style: TextStyle(
+                                color: AppColors.accentColor,
+                                fontWeight: FontWeight.bold)),
+                        TextSpan(text: " volte,"),
+                        TextSpan(text: " contribuendo a ridurre di "),
+                        TextSpan(
+                            text: "456kg",
+                            style: TextStyle(
+                                color: AppColors.accentColor,
+                                fontWeight: FontWeight.bold)),
+                        TextSpan(text: " le emissioni di CO2")
+                      ],
                     ),
-                  ],
-                ),
-              ),
-              const Divider(color: Colors.grey),
-              Row(
-                children: const [
-                  SizedBox(width: 10),
-                  Icon(
-                    Icons.new_label,
-                    color: AppColors.cardColor,
                   ),
-                  SizedBox(width: 10),
-                  Text("Le tue spedizioni"),
-                  Spacer(),
-                  Icon(
-                    Icons.keyboard_arrow_right,
-                    color: AppColors.cardColor,
+                  const SizedBox(
+                    height: 20,
                   ),
+                  const Divider(color: Colors.grey),
+                  GestureDetector(
+                    onTap: () => viewModel.goToPudos(context),
+                    child: Row(
+                      children: const [
+                        SizedBox(width: 10),
+                        Icon(
+                          Icons.person_pin_circle,
+                          color: AppColors.cardColor,
+                        ),
+                        SizedBox(width: 10),
+                        Text("I tuoi pudo"),
+                        Spacer(),
+                        Icon(
+                          Icons.keyboard_arrow_right,
+                          color: AppColors.cardColor,
+                        ),
+                      ],
+                    ),
+                  ),
+                  const Divider(color: Colors.grey),
+                  Row(
+                    children: const [
+                      SizedBox(width: 10),
+                      Icon(
+                        Icons.new_label,
+                        color: AppColors.cardColor,
+                      ),
+                      SizedBox(width: 10),
+                      Text("Le tue spedizioni"),
+                      Spacer(),
+                      Icon(
+                        Icons.keyboard_arrow_right,
+                        color: AppColors.cardColor,
+                      ),
+                    ],
+                  ),
+                  const Divider(color: Colors.grey),
                 ],
-              ),
-              const Divider(color: Colors.grey),
-            ],
-          ),
+              )),
         );
       }),
     );
