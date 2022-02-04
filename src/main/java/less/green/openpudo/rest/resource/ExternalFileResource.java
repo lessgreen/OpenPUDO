ee21ac67c71e2de0e892de2a1df7e716600dac0f
Francesco Magno <francesco@magno.cc>
2022-02-04 12:53:45 +0100

    external file api made public to support share page

--- src/main/java/less/green/openpudo/rest/resource/ExternalFileResource.java
+++ src/main/java/less/green/openpudo/rest/resource/ExternalFileResource.java
@@ -2,6 +2,7 @@ package less.green.openpudo.rest.resource;
 
 import less.green.openpudo.business.service.ExternalFileService;
 import less.green.openpudo.rest.config.annotation.BinaryAPI;
+import less.green.openpudo.rest.config.annotation.PublicAPI;
 import lombok.extern.log4j.Log4j2;
 import org.eclipse.microprofile.openapi.annotations.Operation;
 
@@ -24,6 +25,7 @@ public class ExternalFileResource {
 
     @GET
     @Path("/{fileId}")
+    @PublicAPI
     @BinaryAPI
     @Operation(summary = "Get external file, simulating a static resource served by an http server")
     public Response getExternalFile(@PathParam(value = "fileId") UUID externalFileId) {
