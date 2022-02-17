ac43ec2beb98ee7e1e4d1dcabf33ceb8559ac229
Francesco Magno <francesco@magno.cc>
2022-02-17 18:18:52 +0100

    cosmetics

--- src/main/java/less/green/openpudo/common/ExceptionUtils.java
+++ src/main/java/less/green/openpudo/common/ExceptionUtils.java
@@ -10,6 +10,8 @@ import java.util.stream.Collectors;
 
 public class ExceptionUtils {
 
+    private static final String ROOT_PACKAGE = "less.green.openpudo";
+
     private ExceptionUtils() {
     }
 
@@ -17,14 +19,14 @@ public class ExceptionUtils {
         return throwable.getClass().getSimpleName() + ": " + throwable.getMessage();
     }
 
-    public static String getCanonicalFormWithRootCause(Throwable throwable, Throwable rootCause) {
+    public static String getCanonicalFormWithCause(Throwable throwable, Throwable rootCause) {
         return getCanonicalForm(throwable) + " [caused by " + getCanonicalForm(rootCause) + "]";
     }
 
     public static String getCanonicalFormWithRootCause(Throwable throwable) {
         Throwable rootCause = getRootCause(throwable);
         if (rootCause != null && rootCause != throwable) {
-            return getCanonicalFormWithRootCause(throwable, rootCause);
+            return getCanonicalFormWithCause(throwable, rootCause);
         }
         return getCanonicalForm(throwable);
     }
@@ -56,7 +58,17 @@ public class ExceptionUtils {
 
     public static String getRelevantStackTrace(Throwable throwable) {
         List<String> lines = getStackTraceLines(throwable);
-        return lines.stream().filter(l -> isRelevantLine(l, "less.green.openpudo")).collect(Collectors.joining("\n"));
+        return lines.stream().filter(l -> isRelevantLine(l, ROOT_PACKAGE)).collect(Collectors.joining("\n"));
+    }
+
+    public static String getRelevantStackTrace(Throwable throwable, String prefix) {
+        List<String> lines = getStackTraceLines(throwable);
+        return lines.stream().filter(l -> isRelevantLine(l, prefix)).collect(Collectors.joining("\n"));
+    }
+
+    public static String getRelevantStackTrace(Throwable throwable, List<String> prefixes) {
+        List<String> lines = getStackTraceLines(throwable);
+        return lines.stream().filter(l -> prefixes.stream().anyMatch(pr -> isRelevantLine(l, pr))).collect(Collectors.joining("\n"));
     }
 
     private static boolean isRelevantLine(String line, String prefix) {
