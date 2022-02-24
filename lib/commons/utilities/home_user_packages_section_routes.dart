1dff089ed39396122d694d93b98f1c90a4dbea7e
Francesco La Forgia <tecfra98@gmail.com>
2022-02-24 18:14:20 +0100

    Update missing packagePickupRoute

--- lib/commons/utilities/home_user_packages_section_routes.dart
+++ lib/commons/utilities/home_user_packages_section_routes.dart
@@ -21,26 +21,18 @@
 import 'dart:developer';
 
 import 'package:flutter/cupertino.dart';
-import 'package:latlong2/latlong.dart';
 import 'package:qui_green/app.dart';
 import 'package:qui_green/controllers/home_user_packages.dart';
-import 'package:qui_green/controllers/instruction_controller.dart';
-import 'package:qui_green/controllers/onboarding/insert_address_controller.dart';
-import 'package:qui_green/controllers/onboarding/maps_controller.dart';
 import 'package:qui_green/controllers/package_pickup_controller.dart';
-import 'package:qui_green/controllers/profile_controller.dart';
-import 'package:qui_green/controllers/pudo_detail_controller.dart';
-import 'package:qui_green/controllers/pudo_list_controller.dart';
-import 'package:qui_green/controllers/registration_complete_controller.dart';
-import 'package:qui_green/controllers/user_position_controller.dart';
 import 'package:qui_green/models/pudo_package.dart';
-import 'package:qui_green/models/pudo_profile.dart';
 import 'package:qui_green/resources/routes_enum.dart';
 
 dynamic routeHomeUserPackagesSectionWithSetting(RouteSettings settings) {
   log("current route name: ${settings.name}");
   currentRouteName.value = settings.name ?? '/main';
   switch (settings.name) {
+    case Routes.packagePickup:
+      return CupertinoPageRoute(builder: (context) => PackagePickupController(packageModel: settings.arguments as PudoPackage));
     default:
       return CupertinoPageRoute(
         builder: (context) => const HomeUserPackages(),
