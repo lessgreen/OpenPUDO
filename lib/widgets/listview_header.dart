d635b180daa31cb58d684c133190b165d1ba5da1
valvoline <costantino@sofapps.it>
2022-02-27 20:25:10 +0100

    ListViewHeader: Minor refactoring
    Add CupertinoScrollBar option
    itemBuilder -> contentBuilder to avoid naming collision with parent widget

--- lib/widgets/listview_header.dart
+++ lib/widgets/listview_header.dart
@@ -1,63 +1,75 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:qui_green/resources/res.dart';
 
 class ListViewHeader extends StatelessWidget {
+  final int itemCount;
+  final bool shrinkWrap;
+  final Function(BuildContext, int) contentBuilder;
+  final ScrollPhysics? physics;
+  final String title;
+  final TextStyle? titleStyle;
+  final EdgeInsets itemPadding;
+  final ScrollController? scrollController;
+  final String? endText;
+  final int actualItemCount;
+  final bool hasScrollbar;
   const ListViewHeader({
     Key? key,
     required this.itemCount,
-    this.shrinkWrap = false,
-    required this.itemBuilder,
-    this.physics,
-    this.itemPadding = EdgeInsets.zero,
+    required this.contentBuilder,
     required this.title,
+    this.hasScrollbar = false,
+    this.endText,
+    this.physics,
+    this.titleStyle,
     this.scrollController,
-    required this.endText,
+    this.shrinkWrap = false,
+    this.itemPadding = EdgeInsets.zero,
   })  : actualItemCount = itemCount + 2,
         super(key: key);
 
-  final int itemCount;
-  final bool shrinkWrap;
-  final Function(BuildContext, int) itemBuilder;
-  final ScrollPhysics? physics;
-  final String title;
-  final EdgeInsets itemPadding;
-  final ScrollController? scrollController;
-  final String endText;
-  final int actualItemCount;
-
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
+    var _listView = ListView.builder(
+      itemCount: actualItemCount,
+      physics: physics,
+      shrinkWrap: shrinkWrap,
       controller: scrollController,
       itemBuilder: (context, index) {
         if (index == 0) {
           //Start
           return Container(
-              padding: const EdgeInsets.symmetric(horizontal: Dimension.padding, vertical: Dimension.padding),
-              alignment: Alignment.centerLeft,
-              child: Text(
-                title,
-                style: TextStyle(color: Colors.grey.shade800),
-              ));
+            padding: const EdgeInsets.symmetric(horizontal: Dimension.padding, vertical: Dimension.padding),
+            alignment: Alignment.centerLeft,
+            child: Text(
+              title,
+              style: titleStyle ?? TextStyle(color: Colors.grey.shade800),
+            ),
+          );
         } else if (index == actualItemCount - 1) {
           //No more items
-          return Container(
-              alignment: Alignment.center,
-              child: Text(
-                endText,
-                style: TextStyle(color: Colors.grey.shade800),
-              ));
+          return endText != null
+              ? Container(
+                  alignment: Alignment.center,
+                  child: Text(
+                    endText!,
+                    style: TextStyle(color: Colors.grey.shade800),
+                  ),
+                )
+              : Container(
+                  alignment: Alignment.center,
+                  child: const SizedBox(),
+                );
         } else {
           //Actual item
           return Padding(
             padding: itemPadding,
-            child: itemBuilder(context, index - 1),
+            child: contentBuilder(context, index - 1),
           );
         }
       },
-      itemCount: actualItemCount,
-      physics: physics,
-      shrinkWrap: shrinkWrap,
     );
+    return hasScrollbar ? CupertinoScrollbar(child: _listView) : _listView;
   }
 }
