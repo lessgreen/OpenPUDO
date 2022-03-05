548c4f2afdbb9f9592315e9aa1cbad4e31dedbe0
valvoline <costantino@sofapps.it>
2022-03-05 11:31:59 +0100

    PudoSummary: Add support for customAddress into model

--- lib/models/pudo_summary.g.dart
+++ lib/models/pudo_summary.g.dart
@@ -7,20 +7,22 @@ part of 'pudo_summary.dart';
 // **************************************************************************
 
 PudoSummary _$PudoSummaryFromJson(Map<String, dynamic> json) => PudoSummary(
+      pudoId: json['pudoId'] as int?,
       businessName: json['businessName'] as String,
       label: json['label'] as String?,
-      pudoId: json['pudoId'] as int?,
       pudoPicId: json['pudoPicId'] as String?,
       rating: json['rating'] == null
           ? null
           : RatingModel.fromJson(json['rating'] as Map<String, dynamic>),
+      customizedAddress: json['customizedAddress'] as String?,
     );
 
 Map<String, dynamic> _$PudoSummaryToJson(PudoSummary instance) =>
     <String, dynamic>{
+      'pudoId': instance.pudoId,
       'businessName': instance.businessName,
       'pudoPicId': instance.pudoPicId,
-      'pudoId': instance.pudoId,
       'label': instance.label,
       'rating': instance.rating,
+      'customizedAddress': instance.customizedAddress,
     };
