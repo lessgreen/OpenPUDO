f5110ffd84e81ed1b9b71c84d9de265f20d24208
valvoline <costantino@sofapps.it>
2022-05-09 10:41:28 +0200

    Fix an issue on NetworkHelper that doesn't take into account OPBaseResponse

--- lib/commons/utilities/network_error_helper.dart
+++ lib/commons/utilities/network_error_helper.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/cupertino.dart';
 import 'package:qui_green/commons/alert_dialog.dart';
 import 'package:qui_green/commons/utilities/localization.dart';
+import 'package:qui_green/models/base_response.dart';
 
 class NetworkErrorHelper {
   static void helper(BuildContext context, dynamic value) {
     if (value is ErrorDescription) {
       SAAlertDialog.displayAlertWithClose(context, 'genericErrorTitle'.localized(context, 'general'), value);
+    } else if (value is OPBaseResponse && value.message != null) {
+      SAAlertDialog.displayAlertWithClose(context, 'genericErrorTitle'.localized(context, 'general'), value.message);
     } else {
       SAAlertDialog.displayAlertWithClose(
         context,
